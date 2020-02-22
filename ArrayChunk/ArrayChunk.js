00bbc92de8f7ccccafcde5ef35bc8304e01171f5
Ravi Nivangune <ravinivangune13@gmail.com>
2020-02-22 10:51:55 +0530

    Solved chunk array problem

--- ArrayChunk/ArrayChunk.js
+++ ArrayChunk/ArrayChunk.js
@@ -1,7 +1,16 @@
 const arr = [1,2,,3,4,5,6,7,8]
 
-const chunk =(arr,size)=>{
 
-    
+const chunkArr=[];
+let chunk=[];
+const chunkSize =3;
 
+let index =0;
+while(index<arr.length){
+chunkArr.push(arr.slice(index,index+ chunkSize))
+index += chunkSize;
 }
+
+console.log(chunkArr);
+
+
